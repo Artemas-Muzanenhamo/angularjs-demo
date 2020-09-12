06ba9e64e1070ee40ddda8ceb7f90e5813029c3a
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-09-12 23:56:42 +0100

    refactor movie component

--- client/app/components/movie/movie.component.js
+++ client/app/components/movie/movie.component.js
@@ -3,7 +3,15 @@ import './movie.component.css';
 
 // TODO: use Movie Service
 const MovieComponent = {
-    template
+    templateUrl: 'app/components/movie/movie.component.html',
+    controller: ['$q', MovieController],
+    controllerAs: 'movieCtrl'
+}
+
+function MovieController() {
+    const self = this;
+
+    self.title = () => 'Movie Title';
 }
 
 export default MovieComponent;
