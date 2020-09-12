f505572c3663cee1dfa2b6aaa958ac3da065775a
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-09-12 19:01:48 +0100

    refactor movie component folder name

--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,5 +1,5 @@
 import angular from 'angular';
-import MovieModule from "./movie-component/movie.module";
+import MovieModule from "./movie/movie.module";
 
 const ComponentsModule = angular.module('app.components', [
     MovieModule.name
