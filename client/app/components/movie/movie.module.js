43ad2d857e8591d10da9d9c286316b4f8cdc44ca
Artemas-Muzanenhamo <artemas.muzanenhamo@rightmove.co.uk>
2020-09-13 08:39:13 +0100

    add service

--- client/app/components/movie/movie.module.js
+++ client/app/components/movie/movie.module.js
@@ -1,8 +1,19 @@
 import MovieComponent from "./movie.component";
-import MovieService from "./movie.service";
 
 const MovieModule = angular.module('movie', [])
     .component('movie', MovieComponent)
-    .service('movieService', MovieService);
+    // .service('movieService', MovieService);
+    .service('movieService', function MovieService($http, $q) {
+        let self = this;
+
+        self.getAllMovies = getAllMovies;
+
+        function getAllMovies() {
+            return $http.get('/api/movies')
+                .then((response) => response)
+                .then(response => console.log(response))
+                .catch($q.reject());
+        }
+    });
 
 export default MovieModule;
